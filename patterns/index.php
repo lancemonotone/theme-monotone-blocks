35697f9884720f05be530d147ef068b8002a546c
Rus Miller <lancemonotone@gmail.com>
2024-06-04 12:56:36 -0400

    Commit WP templates.

---
+++ patterns/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: index
+ * Slug: monotone_blocks/index
+ * Categories: hidden
+ * Inserter: no
+ */
+?><?php
+
+?>
+<!-- wp:group {"metadata":{"name":"Site Container Body"},"className":"relative antialiased h-full leading-relaxed text-pretty","layout":{"type":"constrained"}} -->
+<div class="wp-block-group relative antialiased h-full leading-relaxed text-pretty"><!-- wp:monotone-blocks/spotlight /-->
+
+<!-- wp:group {"metadata":{"name":"Site Container Outer"},"className":"z-20 mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0","layout":{"type":"constrained"}} -->
+<div class="wp-block-group z-20 mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0"><!-- wp:group {"metadata":{"name":"Site Container Inner"},"className":"lg:flex lg:gap-4 lg:justify-between","layout":{"type":"constrained"}} -->
+<div class="wp-block-group lg:flex lg:gap-4 lg:justify-between"><!-- wp:monotone-blocks/site-header /-->
+
+<!-- wp:group {"tagName":"main","metadata":{"name":"Main Content"},"className":"pt-24 lg:py-24 lg:w-1/2","layout":{"type":"constrained"}} -->
+<main class="wp-block-group pt-24 lg:py-24 lg:w-1/2"><!-- wp:post-content /-->
+
+<!-- wp:monotone-blocks/site-footer -->
+<!-- wp:paragraph -->
+<p><?php echo __('Designed in <a href="https://www.figma.com/" target="_blank" rel="noreferrer noopener nofollow">Figma</a> and coded in <a href="https://www.jetbrains.com/phpstorm/" target="_blank" rel="noreferrer noopener nofollow">PhpStorm</a> and <a href="https://pinegrow.com/" data-type="link" data-id="https://pinegrow.com/" target="_blank" rel="noreferrer noopener nofollow">Pinegrow</a> by yours truly. Styled with <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer noopener nofollow">Tailwind CSS</a>.', 'monotone_blocks');?></p>
+<!-- /wp:paragraph -->
+<!-- /wp:monotone-blocks/site-footer --></main>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
