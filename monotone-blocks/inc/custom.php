d4d06f8bd5e8aa6068d694d1654c139b6f4f0eee
Rus Miller <lancemonotone@gmail.com>
2024-06-24 12:06:27 -0400

    Readmore filter.

--- monotone-blocks/inc/custom.php
+++ monotone-blocks/inc/custom.php
@@ -9,4 +9,8 @@
         wp_enqueue_script( 'monotone_blocks-theme', get_template_directory_uri() . '/js/theme.js', null, '1.0.59', true );
         }
     } );
-    ?>
+
+    add_filter('excerpt_more', function($more){
+        global $post;
+        return '... <a href="'. get_permalink($post->ID) . '" class="read-more">Read More</a>';
+    });
